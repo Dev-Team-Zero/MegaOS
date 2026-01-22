7ce3a0dd1fdd6e960be93a2110240ebc4520300c
Victor <albu.victor.m@gmail.com>
2026-01-22 14:36:13 +0200

    idk

--- kernel/arch/x86_64/boot/boot.asm
+++ kernel/arch/x86_64/boot/boot.asm
@@ -13,6 +13,8 @@ extern long_mode_start
 .long CHECKSUM
 
 .section .bss
+.align 4096
+l4: resb 4096
 .align 16
 stack_bottom:
     .skip 16384
@@ -21,6 +23,22 @@ stack_top:
 .section .text
 .global _start
 .type _start, @function
+bits 32
 _start:
-    mov $stack_top, %esp
-    call long_mode_start
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    mov eax, l4
+    mov cr3, eax
+
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    jmp 0x08:long_mode_start
